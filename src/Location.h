f891dc36bbe99dffccac447a1453e4ac5e014e9a
Lazarus Turpaud <pibob314@gmail.com>
2023-10-07 01:46:19 -0700

    redo directories

---
+++ src/Location.h
@@ -0,0 +1,12 @@
+//Location_h
+#ifndef Location_H
+#define Location_H
+
+typedef struct {
+  double latitude;
+  double longitude;
+  int elevation;
+  unsigned long long time;
+} Location;
+
+#endif
---
+++ src/Track_Seg.h
@@ -0,0 +1,20 @@
+#include "Location.h"
+
+//Track_Seg
+#ifndef Track_Seg_H
+#define Track_Seg_H
+
+typedef struct {
+  int elevation_gain;
+  int elevation_loss;
+} Elevation;
+
+typedef struct {
+  Location *locations;
+  double distance;
+  unsigned long long time;
+  Elevation elevation;
+} Track_Seg;
+
+
+#endif // !Track_Seg_H
